8ef15441f774287d0ec74a5520effbdcc9bd2020
morgan-legare <morganlegare1@yahoo.com>
2025-05-08 22:00:47 -0700

    Added the main menu

--- main.cpp
+++ main.cpp
@@ -3,8 +3,26 @@
 #include <iostream>
 #include "npc.h"
 #include "game.h"
+using namespace std;
+
+void printMainMenu();
 
 int main() {
-
+	printMainMenu();
 	return 0;
+}
+
+
+void printMainMenu() {
+	cout <<"\n(=========================================)\n" 
+		"\n     (==============================)\n"
+		  "      WELCOME TO TERMINAL-ADVENTURES \n"
+		  "     (==============================)\n";
+	cout << "\n     WHAT WOULD YOU LIKE TO DO FIRST?\n"
+		"\n(=========================================)\n"
+		"\n 1) MAKE A NEW CHARACTER\n"
+		" 2) START WITH A PRE-MADE CHARACTER\n"
+		" 3) READ THE GUIDE\n"
+		" 4) SAVE AND QUIT\n" 
+		"\n(=========================================)\n";
 }
